131c234872cdca5ab0564afd6092eb9d5e36a6e4
cheng hao <chenghaochua88@gmail.com>
2026-05-13 23:45:33 +0800

    Add UOB tracker

--- src/main.py
+++ src/main.py
@@ -1,23 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import yfinance as yf
 
 from strategy import add_moving_averages, generate_signals
 from metrics import calculate_returns
 
+ticker = "U11.SI"
 
-df = pd.read_csv("data/prices.csv")
-df["Date"] = pd.to_datetime(df["Date"])
+df = yf.download(ticker, start="2020-01-01", end="2026-01-01")
 
-df = add_moving_averages(df)
+df = df.reset_index()
+
+df = add_moving_averages(df, short_window = 20, long_window = 50)
 df = generate_signals(df)
 df = calculate_returns(df)
 
-print(df)
+print(df.tail())
 
 plt.plot(df["Date"], df["market_value"], label="Buy and Hold")
 plt.plot(df["Date"], df["strategy_value"], label="Moving Average Strategy")
 plt.legend()
-plt.title("Backtest Results")
+plt.title(f"{ticker} Backtest Results")
 plt.xlabel("Date")
 plt.ylabel("Portfolio Value")
 plt.show()
